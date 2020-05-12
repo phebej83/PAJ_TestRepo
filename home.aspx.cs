705402c615d9c0753c948a3fa21ee528ee365060
phebej83 <65244388+phebej83@users.noreply.github.com>
2020-05-12 12:56:48 -0500

    Added 3 EWIR User Mtg Files
    Still testing GitHub. I added 3 software files

---
+++ home.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace conference
+{
+    public partial class home : System.Web.UI.Page
+    {
+        string network = ConfigurationManager.AppSettings["network"];
+        string SSOPOC = ConfigurationManager.AppSettings["conf_ssoPOC"];
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
